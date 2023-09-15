fa8e53e08a2a4e21c7ea315984120fe16a702e60
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-15 14:22:17 +0100

    Updated the lib to have subtraction function

--- libs/numbers.js
+++ libs/numbers.js
@@ -5,10 +5,21 @@ I would write tests for them in the tests/numbers.test.js file.
 */
 
 const add = (a, b) => {
-  if (!a || !b) throw new Error("Missing arguments");
+  if (!a || !b ) throw new Error("Invalid arguments");
   return a + b;
 };
 
+
+const subtract = (a, b) =>{
+  if (!a || !b ) throw new Error("Invalid arguments");
+
+  return a - b;
+
+}
+
+
 module.exports = {
   add,
+  subtract,
 };
+
--- tests/numbers.test.js
+++ tests/numbers.test.js
@@ -1,25 +1,26 @@
-const numbers = require("../libs/numbers");
+const {add, subtract} = require("./../libs/numbers");
 
-// Detailed test to test all edge cases for adding 2 integers
-describe("Add two integers", () => {
-  it("Adds 2 + 2 and expects the value equal 4", () => {
-    const value = numbers.add(2, 2);
-    expect(value).toEqual(4);
-  });
-
-  it("Adds undefined + undefined and expects an error", () => {
-    expect(() => {
-      numbers.add(undefined, undefined).throwError(Error);
+describe("numbers", () =>{
+    it("should add a number to the other", () =>{
+        const result = add(2, 2);
+        expect(result).toEqual(4);
     });
-  });
 
-  it("Adds null + null and expects an error", () => {
-    expect(() => {
-      numbers.add(null, null).throwError(Error);
+    it("should throw an error if argument is not a number", () =>{
+     expect(() => {
+        add(undefined, undefined)
+     }).toThrow();
     });
-  });
-  it("Adds two floating point numbers - adds 3.2 + 2 to be close to 5.2", () => {
-    const value = numbers.add(3.2, 2);
-    expect(value).toBeCloseTo(5.2);
-  });
-});
+
+    it("should subtract a number from the other", () =>{
+        const result = subtract(4, 2);
+        expect(result).toBe(2);
+    });
+
+    it("should throw an error if argument is not a number", ()=>{
+        expect(() =>{
+            subtract(undefined, undefined);
+        }).toThrow();
+    });
+    
+})
