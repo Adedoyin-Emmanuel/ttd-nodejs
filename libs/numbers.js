fa1db4421086c2972d413c23fa027dd14b061dab
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-15 14:54:43 +0100

    Wrote more tests

--- libs/numbers.js
+++ libs/numbers.js
@@ -5,21 +5,26 @@ I would write tests for them in the tests/numbers.test.js file.
 */
 
 const add = (a, b) => {
-  if (!a || !b ) throw new Error("Invalid arguments");
+  if (!a || !b) throw new Error("Invalid arguments");
   return a + b;
 };
 
-
-const subtract = (a, b) =>{
-  if (!a || !b ) throw new Error("Invalid arguments");
+const subtract = (a, b) => {
+  if (!a || !b) throw new Error("Invalid arguments");
 
   return a - b;
+};
 
-}
+const absolute = (number) => {
+  if (isNaN(number)) throw new Error("Invalid arguments");
 
+  if (number === 0) return number;
+
+  return number < 0 ? -number : number;
+};
 
 module.exports = {
   add,
   subtract,
+  absolute,
 };
-
--- tests/numbers.test.js
+++ tests/numbers.test.js
@@ -1,26 +1,49 @@
-const {add, subtract} = require("./../libs/numbers");
+const { add, subtract, absolute } = require("./../libs/numbers");
 
-describe("numbers", () =>{
-    it("should add a number to the other", () =>{
-        const result = add(2, 2);
-        expect(result).toEqual(4);
-    });
+describe("numbers", () => {
+  it("should add a number to the other", () => {
+    const result = add(2, 2);
+    expect(result).toEqual(4);
+  });
 
-    it("should throw an error if argument is not a number", () =>{
-     expect(() => {
-        add(undefined, undefined)
-     }).toThrow();
-    });
+  it("should throw an error if argument is not a number", () => {
+    expect(() => {
+      add(undefined, undefined);
+    }).toThrow();
+  });
 
-    it("should subtract a number from the other", () =>{
-        const result = subtract(4, 2);
-        expect(result).toBe(2);
-    });
+  it("should subtract a number from the other", () => {
+    const result = subtract(4, 2);
+    expect(result).toBe(2);
+  });
 
-    it("should throw an error if argument is not a number", ()=>{
-        expect(() =>{
-            subtract(undefined, undefined);
-        }).toThrow();
-    });
-    
-})
+  it("should throw an error if argument is not a number", () => {
+    expect(() => {
+      subtract(undefined, undefined);
+    }).toThrow();
+  });
+
+  it("should throw an error if argument is not a number", () => {
+    expect(() => {
+      absolute("foo");
+    }).toThrow();
+  });
+
+  it("should return an equal positive number if a negative number is passed", () => {
+    const result = absolute(-28);
+
+    expect(result).toEqual(28);
+  });
+
+  it("should return a positive number if a positive number is passed", () => {
+    const result = absolute(28);
+    expect(result).toBe(28);
+  });
+
+  it("should return 0 if 0 is passed as the argument", () => {
+    const result = absolute(0);
+    expect(result).toBe(0);
+  });
+
+
+});
